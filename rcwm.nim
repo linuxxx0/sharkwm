e6bc12a14d76a2de9eae418ddb65aad089feb6d1
linux <94711667+linuxxx0@users.noreply.github.com>
2022-07-18 13:19:28 +0300

    add get func(alpha)

---
+++ rcwm.nim
@@ -0,0 +1,29 @@
+# rcwm is a light, fast and easy to use window manager
+#     ___  _____
+#    / _ \/ ___/     ____ _
+#   / , _/ /__ | |/|/ /  ' \
+#  /_/|_|\___/|__,__/_/_/_/
+
+import
+  std/[options, os, sequtils, strutils, parsecfg],
+  x11/[xlib, x, xft, xatom, xinerama, xrender],
+  types,
+  atoms,
+  log,
+  pixie,
+  regex
+
+converter toXBool*(x: bool): XBool = x.XBool
+converter toBool*(x: XBool): bool = x.bool
+
+proc getResolution*(): string =
+  let
+    Resulution: Config = loadConfig("xrandr -d :0")
+
+  result = Resulution.getSectionValue("", "current")
+
+proc getDistroId*(): string =
+  let
+    osRelease: Config = loadConfig("/etc/os-release")
+
+  result = osRelease.getSectionValue("", "ID")
