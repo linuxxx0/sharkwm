7859b55de6238da566fb1e75f6c3d947c66c4379
linux <94711667+linuxxx0@users.noreply.github.com>
2022-07-18 21:48:52 +0300

    file structure change

--- sharkwm.nim
+++ sharkwm.nim
@@ -1,22 +1,37 @@
-# rcwm is a light, fast and easy to use window manager
+# sharkwm is a light, fast and easy to use window manager
 #
 #         __            __  _      ____  ___
 #    ___ / /  ___ _____/ /_| | /| / /  |/  /
 #   (_-</ _ \/ _ `/ __/  '_/ |/ |/ / /|_/ /
 #  /___/_//_/\_,_/_/ /_/\_\|__/|__/_/  /_/
 
+ import
+    x11/xlib,
+    x11/xutil,
+    x11/x
+
 import
-  std/options, os, sequtils, strutils, parsecfg,
-  x11/xlib, x, xft, xatom, xinerama, xrender,
+    std/parsecfg,
+    std/strutils
 
 converter toXBool*(x: bool): XBool = x.XBool,
 converter toBool*(x: XBool): bool = x.bool,
 
-proc getResolution*(): string =
-  let
-    Resulution: Config = loadConfig("xrandr -d :0")
+proc pass =
+  discard
 
-  result = Resulution.getSectionValue("", "current")
+type Bar = object
+
+type XWindInfo = object
+  display*: PDisplay
+  attr*: TXWindowAttributes
+  start*: TXButtonEvent
+  ev*: TXEvent
+
+proc initXWindInfo(windInfo var XWindInfo): XWindInfo =
+  display = XOpenDisplay(nil)
+  if display == nil:
+    quit "I can't see your display"
 
 proc getDistro*(): string =
   let
@@ -28,16 +43,16 @@ proc getMemory*(): string =
   let
      fileSeq: seq[string] = readLines("/proc/meminfo", 3)
 
-   let
-     memTotalSeq: seq[string] = fileSeq[0].split(" ")
-     memAvailableSeq: seq[string] = fileSeq[2].split(" ")
+  let
+    memTotalSeq: seq[string] = fileSeq[0].split(" ")
+    memAvailableSeq: seq[string] = fileSeq[2].split(" ")
 
-     memTotalInt: int = parseInt(memTotalSeq[^2]) div 1024
-     memAvailableInt: int = parseInt(memAvailableSeq[^2]) div 1024
+    memTotalInt: int = parseInt(memTotalSeq[^2]) div 1024
+    memAvailableInt: int = parseInt(memAvailableSeq[^2]) div 1024
 
-     memUsedInt: int = memTotalInt - memAvailableInt
+    memUsedInt: int = memTotalInt - memAvailableInt
 
-   result = $(memUsedInt) & " | " & $(memTotalInt) & " MiB"
+  result = $(memUsedInt) & " | " & $(memTotalInt) & " MiB"
 
 proc getWM*(): string =
   let
